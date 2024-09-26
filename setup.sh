750ac7199ac07208e51f32ca4b0dc541f3a19aa9
Mr CJG Otter <27728277@su@cld-495545.cs.sun.ac.za>
2024-09-26 15:34:41 +0200

    updated setup file to pull a list of country codes

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ mkdir data
 wget https://databank.worldbank.org/data/download/EdStats_CSV.zip
 unzip -u -d ./data EdStats_CSV.zip
 rm EdStats_CSV.zip
+
+wget -O ./data/countries.csv https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv
