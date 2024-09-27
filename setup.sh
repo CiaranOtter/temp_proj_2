a40a10666656f04a93e9f5d7bcefc1667f24c2a9
Ciaran Otter <ciaranjotter@gmail.com>
2024-09-27 13:53:44 +0200

    adding changes before moving to PC

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir data
-wget https://databank.worldbank.org/data/download/EdStats_CSV.zip
-unzip -u -d ./data EdStats_CSV.zip
-rm EdStats_CSV.zip
+wget https://archive.ics.uci.edu/static/public/2/adult.zip
+unzip -u -d ./data adult.zip
+rm adult.zip
 
-wget -O ./data/countries.csv https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv
+# wget -O ./data/countries.csv https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv
