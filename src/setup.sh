87500b0e3c343fff5fd98180f878f224792d0597
Mr CJG Otter <27728277@su@cld-495545.cs.sun.ac.za>
2024-09-30 17:51:05 +0200

    adding work to temp space

--- src/setup.sh
+++ src/setup.sh
@@ -1,4 +1,9 @@
-mkdir data
-mkdir ../report/data
-mkdir ../report/images
+for i in 1 2 3 4 5
+do
+    cd ../report/hypothesis_$i
+    mkdir data
+    mkdir images
+    cd ../../src
+done
+
 unzip -u -d ../data ../data/GHS-2023-PERSON.zip
