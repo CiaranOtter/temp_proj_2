fc729929d57419b173eaf79c8a3d634b3e2c28aa
Ciaran Otter <ciaranjotter@gmail.com>
2024-09-30 07:13:42 +0200

    small update to file structure to meet required format

--- setup.sh
+++
@@ -1,5 +0,0 @@
-mkdir data
-mkdir report/data
-mkdir report/images
-unzip -u -d ./data data/GHS-2023-PERSON.zip
-# wget -O ./data/countries.csv https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/refs/heads/master/all/all.csv
---
+++ src/setup.sh
@@ -0,0 +1,4 @@
+mkdir data
+mkdir ../report/data
+mkdir ../report/images
+unzip -u -d ../data ../data/GHS-2023-PERSON.zip
